0b689b2daffaa5278df58e2f6d9b4a62357719fc
dc morrison <nosirromd@gmail.com>
2022-05-04 23:57:15 +0100

    inject connection string; delete weather forecasting code; deploy IConnectionmultiplexer

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using StackExchange.Redis;
 
 namespace RedisAPI
 {
@@ -26,7 +27,8 @@ namespace RedisAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddSingleton<IConnectionMultiplexer>(opt =>
+                ConnectionMultiplexer.Connect(Configuration.GetConnectionString("RedisConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
